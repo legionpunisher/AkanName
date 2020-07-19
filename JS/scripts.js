6e12d586370f20da00ef5377577f78fce7295339
legionpunisher <mbuguamoses656@gmail.com>
2020-07-19 23:30:38 +0300

    added a variable added an array added a function to evaluate the content of the js script page

---
+++ JS/scripts.js
@@ -0,0 +1,11 @@
+function details() {
+    //variables and arrays
+    var femaleAkanName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    var maleAkanName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thusday", "Friday", "Saturday"];
+    var gender = document.getElementById("gender");
+    var CC = document.getElementById("century");
+    var YY = document.getElementById("year");
+    var MM = document.getElementById("month");
+    var DD = document.getElementById("date");
+    //validating date and month of birth
